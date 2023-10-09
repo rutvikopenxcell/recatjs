d06cb70ef542683359c568ca2dd0a918a2161de1
rutvik <rutvik.mohite@openxcellinc.com>
2023-10-09 19:56:16 +0530

    dsds

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -4,31 +4,32 @@ import { setUserSession } from '../utils/common';
 import axios from 'axios';
 
 const Join = () => {
-     const [error, setError] = useState(null);
-     const [loading, setLoading] = useState(false);
-     const history = useNavigate();
-     const handleSubmit=(e)=>{
-     e.preventDefault();
-     const name = e.target.name.value
-     const email = e.target.email.value
-     const text = e.target.message.value
-     console.log(name)
-     axios.post('http://localhost:4000/users/signin', { name: name, email: email}).then(response => {
-      setLoading(false);
-      setUserSession(response.data.token, response.data.user);
-      history('/dashboard');
-    }).catch(error => {
-      setLoading(false);
-      if (error.response.status === 401) setError(error.response.data.message);
-      else setError("Something went wrong. Please try again later.");
-    });
-    }
-    const handleChange=(e)=>{
+   const [error, setError] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const history = useNavigate();
+   const handleSubmit = (e) => {
       e.preventDefault();
-      const name=e.target.name;
-      const value=e.target.value;
+      const name = e.target.name.value
+      const email = e.target.email.value
+      const message = e.target.message.value
+      console.log(name)
+      axios.post('http://127.0.0.1:8000/api/contact/store', { name: name, email: email, message: message }).then(response => {
+         setLoading(false);
+         console.log(response)
+         setUserSession(response.data.token, response.data.user);
+         history('/dashboard');
+      }).catch(error => {
+         setLoading(false);
+         if (error.response.status === 401) setError(error.response.data.message);
+         else setError("Something went wrong. Please try again later.");
+      });
+   }
+   const handleChange = (e) => {
+      e.preventDefault();
+      const name = e.target.name;
+      const value = e.target.value;
       console.log(value)
-      
+
       // let error='',errors='';
       // if (name==="name" && value.trim()===''){
       //   error=<strong style={{color:'red'}}>*{name} is required</strong>;
@@ -43,28 +44,28 @@ const Join = () => {
       // this.setState({
       //   [name]:value
       // });
-    }
-return <>
-<div class="contact_section layout_padding">
-      <div class="container">
-         <div class="contact_section_2">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="mail_section_1">
-                     <h1 class="contact_taital">Contact Us</h1>
-                     <form onSubmit={handleSubmit} method="post">
-                     <input type="text" class="mail_text_1" placeholder="Name" name="name" onChange={handleChange}/>
-                     <input type="text" class="mail_text_1" placeholder="Email" name="email" onChange={handleChange}/>
-                     <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="message" onChange={handleChange}></textarea>
-                     <button>Submit</button>
-                     </form>
+   }
+   return <>
+      <div class="contact_section layout_padding">
+         <div class="container">
+            <div class="contact_section_2">
+               <div class="row">
+                  <div class="col-md-12">
+                     <div class="mail_section_1">
+                        <h1 class="contact_taital">Contact Us</h1>
+                        <form onSubmit={handleSubmit} method="post">
+                           <input type="text" class="mail_text_1" placeholder="Name" name="name" onChange={handleChange} />
+                           <input type="text" class="mail_text_1" placeholder="Email" name="email" onChange={handleChange} />
+                           <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="message" onChange={handleChange}></textarea>
+                           <button className="btn btn-danger">Submit</button>
+                        </form>
+                     </div>
                   </div>
                </div>
             </div>
          </div>
       </div>
-   </div>
-    </>;
-  };
-  
-  export default Join;
+   </>;
+};
+
+export default Join;
